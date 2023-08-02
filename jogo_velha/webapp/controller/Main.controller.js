1d874b78470bb3930d896e0854eacb0bcf508b19
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2023-08-01 21:14:12 -0300

    aula 1

--- jogo_velha/webapp/controller/Main.controller.js
+++ jogo_velha/webapp/controller/Main.controller.js
@@ -6,9 +6,31 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
-
         return Controller.extend("jogovelha.controller.Main", {
             onInit: function () {
+                this.vez = 'X'
+
+            },
+
+            onClickCasa: function(oEvent) {
+                //obter referencia do objeto que foi clicado
+                let casa = oEvent.getSource()
+
+                // obter imagem atual
+                let imagem = casa.getSrc()
+
+                // verificar se imagem e branco
+                if (imagem == "../img/Branco.png") {
+
+                    //comando para adicionar imagem
+                    casa.setSrc("../img/" + this.vez + ".png");
+
+                    if (this.vez == 'X') {
+                        this.vez = 'O'
+                    } else {
+                        this.vez = 'X'
+                    }
+                }
 
             }
         });
